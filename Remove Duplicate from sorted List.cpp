a2316d21bfd89b1cd9040fefd039cda914848392
Ritika Saha <72276256+ritika-saha@users.noreply.github.com>
2022-03-10 22:16:01 +0530

    Runtime 8ms

--- Remove Duplicate from sorted List.cpp
+++ Remove Duplicate from sorted List.cpp
@@ -1,3 +1,13 @@
+
+/*LeetCode #82 
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. 
+Return the linked list sorted as well.
+
+eg- Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+**/
+
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -21,3 +31,7 @@ public:
         return dummy->next;
     }
 };
+
+/*Note- created a dummy node to store the ultimate answer's pointer, ran a loop from begin to end of given list and in the nested loop incremented it until no duplicate was found
+for the current element . If the current node is not duplicate then added it to the answer list and updated the prev to the current .....else if it is duplicate then updated 
+prev's next to the current's next**/
