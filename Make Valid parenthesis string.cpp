66ee5ea8414fd13528c410f50c1e157cd7e3b0c9
Ritika Saha <72276256+ritika-saha@users.noreply.github.com>
2022-03-28 23:35:31 +0530

    Update Make Valid parenthesis string.cpp

--- Make Valid parenthesis string.cpp
+++ Make Valid parenthesis string.cpp
@@ -1,5 +1,5 @@
 /**LeetCode 1249 MINIMUN REMOVE TO MAKE VALID PARENTHESIS
-Given a string s of '(' , ')' and lowercase English characters.
+Given a string 's' of '(' , ')' and lowercase English characters.
 
 Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
 
