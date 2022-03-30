8f4806d7ef8aa3db52b0a283da53aff9416504ce
Ritika Saha <72276256+ritika-saha@users.noreply.github.com>
2022-03-30 23:49:27 +0530

    Create search in 2d matrix.cpp

---
+++ search in 2d matrix.cpp
@@ -0,0 +1,11 @@
+/**LEETCODE 74
+
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+
+examples---
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+*
