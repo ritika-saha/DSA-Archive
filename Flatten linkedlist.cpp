b50ec5932011a4f890090fbcb9ecef39c9467482
Ritika Saha <72276256+ritika-saha@users.noreply.github.com>
2022-10-15 09:27:46 +0530

    Create Flatten linkedlist.cpp

---
+++ Flatten linkedlist.cpp
@@ -0,0 +1,72 @@
+/**
+Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
+(i) a next pointer to the next node,
+(ii) a bottom pointer to a linked list where this node is head.
+Each of the sub-linked-list is in sorted order.
+Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order. 
+Note: The flattened list will be printed using the bottom pointer instead of next pointer. For more clearity have a 
+look at the printList() function in the driver code.
+
+ 
+
+Example 1:
+
+Input:
+5 -> 10 -> 19 -> 28
+|     |     |     | 
+7     20    22   35
+|           |     | 
+8          50    40
+|                 | 
+30               45
+Output:  5-> 7-> 8- > 10 -> 19-> 20->
+22-> 28-> 30-> 35-> 40-> 45-> 50.
+*/
+
+ 
+
+Node* mergeTwoLists(Node* a, Node* b) {
+    
+    Node *temp = new Node(0);
+    Node *res = temp; 
+    
+    while(a != NULL && b != NULL) {
+        if(a->data < b->data) {
+            temp->bottom = a; 
+            temp = temp->bottom; 
+            a = a->bottom; 
+        }
+        else {
+            temp->bottom = b;
+            temp = temp->bottom; 
+            b = b->bottom; 
+        }
+    }
+    
+    if(a) temp->bottom = a; 
+    else temp->bottom = b; 
+    
+    return res -> bottom; 
+    
+}
+Node *flatten(Node *root)
+{
+   
+        if (root == NULL || root->next == NULL) 
+            return root; 
+  
+        // recur for list on right 
+        root->next = flatten(root->next); 
+  
+        // now merge 
+        root = mergeTwoLists(root, root->next); 
+  
+        // return the root 
+        // it will be in turn merged with its left 
+        return root; 
+}
+
+
+//BRUTE FORCE- USING PRIORITY QUEUE MIN HEAP STORING PAIR OF NODE VALUE, NODE THEN JOINGING ALL NODES ACCROFING TO PRIORITY
+
+//OPTIMAL APPROACH- USING RECURSION AND MERGING TWO LISTS ...IF THERE ARE 3 LISTS L1 L2 L3 THEN WE MERGE L1 AND L2 TO FORM L4 THEN MERGE L4 AND L3 TO GEF FINAL LIST
