0168015e212dbc603aed033b0a21afef88897a10
Ritika Saha <72276256+ritika-saha@users.noreply.github.com>
2023-09-02 03:23:54 +0530

    Update BST to greater Tree.cpp

--- BST to greater Tree.cpp
+++ BST to greater Tree.cpp
@@ -1,3 +1,5 @@
+// Tags: BST
+
 /*
 Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is 
 changed to the original key plus the sum of all keys greater than the original key in BST.
